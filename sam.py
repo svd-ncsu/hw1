b6d963092cc20f534b72963925ddf4b7345a51d1
Devanshu Shah <92639289+devanshu-777@users.noreply.github.com>
2024-08-28 16:39:04 +0000

    Updated sam.py and test.py

--- sam.py
+++ sam.py
@@ -1,2 +1,10 @@
 def do_smtg(x):
-    return x + 1
+    if x < 0:
+        return "Invalid input"
+    elif x == 0 or x == 1:
+        return x
+    
+    a, b = 0, 1
+    for _ in range(2, x):
+        a, b = b, a + b
+    return b
